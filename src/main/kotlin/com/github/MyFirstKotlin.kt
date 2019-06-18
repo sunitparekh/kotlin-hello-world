279347c90dd433d02120354293471dfb2e440f41
Sunit Parekh <parekh.sunit@gmail.com>
2019-06-18 12:19:01 +0530

    added movies.csv

--- src/main/kotlin/com/github/MyFirstKotlin.kt
+++ src/main/kotlin/com/github/MyFirstKotlin.kt
@@ -2,11 +2,31 @@ package com.github
 
 
 fun main(args : Array<String>) {
-    println("Hello, World!")
 
-    val range = 1..10
-    range.forEach { println(it) }
+    val employee = Employee("Sunit", "Parekh")
+    employee.fullName()
+
+
+    val listOf = listOf("Sunit", "Parekh")
+    listOf.count { item -> item == "Sunit" }
+}
+
+open class Employee(val firstName: String, val lastName: String) {
+
+    var fullName = firstName + lastName
+        set(value) { field = value }
+
+    init {
+        fullName = firstName + lastName
+    }
+
+    open fun fullName() = firstName + lastName
 
 }
 
+class Student(firstName: String, lastName: String, val department: String)
+    : Employee(firstName,lastName) {
 
+
+    override fun fullName() = firstName + lastName
+}
